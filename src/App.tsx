e63b6479801ce839a93590a55c44e043e148681e
HAKJOON0223 <2hakjoon@gmail.com>
2022-02-19 21:52:56 +0900

    add post screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from 'screen/Home/HomeScreen';
 import LoginScreen from 'screen/Login/LoginScreen';
+import PostScreen from 'screen/Post/PostScreen';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomeScreen />} />
-        <Route path="/" element={<LoginScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <Route path="/post" element={<PostScreen />} />
       </Routes>
     </Router>
   );
---
+++ src/screen/Post/PostScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PostScreen() {
+  return <div>Post </div>;
+}
+
+export default PostScreen;
