33051f14b448b97b7644f6c776c4b7e2be4d119f
HAKJOON0223 <2hakjoon@gmail.com>
2022-02-19 21:49:36 +0900

    add router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomeScreen from 'screen/Home/HomeScreen';
+import LoginScreen from 'screen/Login/LoginScreen';
 
 function App() {
-  return <div className="App">asdfsafd</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/" element={<LoginScreen />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
