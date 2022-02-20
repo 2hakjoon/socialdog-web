e43b174e38750b7cc9a9721c549493580e16b56a
HAKJOON0223 <2hakjoon@gmail.com>
2022-02-20 16:40:46 +0900

    add colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,16 @@
 const color = {
-  "white" : "#FFFFFF"
+  blue:{
+    primaryBlue: '#2962ff',
+  },
+  white:{
+    primaryWhite: '#f5f5f5',
+  },
+  achromatic:{
+    whitle: '#FFFFFF',
+    black: '#212121',
+    lightGray: '#e0e0e0',
+    darkGray : "#929292",
+  },
 }
 
 export const theme = {
