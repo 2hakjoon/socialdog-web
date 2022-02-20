93e32df443164e356c9e626898042594a782697f
HAKJOON0223 <2hakjoon@gmail.com>
2022-02-20 22:56:05 +0900

    css homescreen media query

--- src/screen/Home/HomeScreen.tsx
+++ src/screen/Home/HomeScreen.tsx
@@ -4,9 +4,23 @@ import BaseWrapper from 'screen/common-comp/wrappers/BaseWrapper';
 import styled from 'styled-components';
 import PostCard from './components/PostCard';
 
-const PostsWrapper = styled.div`
-  width: 70%;
-  height: 100%;
+const SectionWrapper = styled.div`
+  @media (max-width: 999px) {
+    > :nth-child(1) {
+      width: 100%;
+    }
+    > :nth-child(2) {
+      display: none;
+    }
+  }
+  @media (min-width: 1000px) {
+    > :nth-child(1) {
+      width: 70%;
+    }
+    > :nth-child(2) {
+      width: 30%;
+    }
+  }
 `;
 
 function HomeScreen() {
@@ -14,9 +28,9 @@ function HomeScreen() {
     <>
       <MainHeader />
       <BaseWrapper>
-        <PostsWrapper>
+        <SectionWrapper>
           <PostCard />
-        </PostsWrapper>
+        </SectionWrapper>
       </BaseWrapper>
     </>
   );
