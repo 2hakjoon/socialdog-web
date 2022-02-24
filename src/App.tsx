bfb6d3fe91286d99b78950e75b2b433e69eb0ce8
HAKJOON0223 <2hakjoon@gmail.com>
2022-02-24 22:52:12 +0900

    add apollo

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,42 @@ import LoginScreen from 'screen/Login/LoginScreen';
 import PostScreen from 'screen/Post/PostScreen';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/styles/theme';
+import { ApolloClient, HttpLink, ApolloLink, InMemoryCache, concat, ApolloProvider } from '@apollo/client';
 
 window.Kakao.init(process.env.REACT_APP_KAKAO_LOGIN_API_KEY);
 window.Kakao.isInitialized();
 
+const httpLink = new HttpLink({ uri: 'http://121.154.94.120/graphql' });
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+      authorization: localStorage.getItem('token') || null,
+    },
+  }));
+
+  return forward(operation);
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: concat(authMiddleware, httpLink),
+});
+
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/post" element={<PostScreen />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/post" element={<PostScreen />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
 
