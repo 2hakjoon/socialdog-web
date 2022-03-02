b14f579cdac17f6fc28c4681fb8df26f2180d226
HAKJOON0223 <2hakjoon@gmail.com>
2022-03-02 22:02:45 +0900

    add icons and style layouts

--- src/screen/common-comp/header/MainHeader.tsx
+++ src/screen/common-comp/header/MainHeader.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { routes } from 'screen/routes';
 import styled from 'styled-components';
 import { getAccessToken } from 'utils/local-storage';
-import socialDogLogoBlue from '../../../assets/svg/social-dog-blue.svg';
 import socialDogLogoBlack from '../../../assets/svg/social-dog-black.svg';
 
 const Wrapper = styled.header`
@@ -11,25 +10,33 @@ const Wrapper = styled.header`
   height: 60px;
   background-color: white;
   border-bottom: 2px solid ${({ theme }) => theme.color.achromatic.lightGray};
+  padding: 0 8px;
 `;
 
 const InnerWrapper = styled.div`
   max-width: ${({ theme }) => theme.layout.screenMaxWidth};
   width: 100%;
+  height: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
   margin: 0px auto;
 `;
 
+const LogoWrapper = styled.div`
+  width: 130px;
+  height: 50px;
+  overflow: hidden;
+`;
+
 function MainHeader() {
   return (
     <Wrapper>
       <InnerWrapper>
-        <img alt="asdf" src={socialDogLogoBlue} />
-        <img alt="asdf" src={socialDogLogoBlack} />
         <Link to={routes.home}>
-          <div>홈으로</div>
+          <LogoWrapper>
+            <img alt="asdf" src={socialDogLogoBlack} />
+          </LogoWrapper>
         </Link>
         {getAccessToken() ? (
           <Link to={routes.profile}>
