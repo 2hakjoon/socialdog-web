61c2714a5e5254eee6ac0993e3fd44bcae72c48d
HAKJOON0223 <2hakjoon@gmail.com>
2022-03-10 19:31:54 +0900

    delete error

--- src/__generated__/QMeAll.ts
+++ src/__generated__/QMeAll.ts
@@ -3,6 +3,7 @@
 // @generated
 // This file was automatically generated and should not be edited.
 
+
 // ====================================================
 // GraphQL query operation: QMeAll
 // ====================================================
@@ -11,6 +12,10 @@ export interface QMeAll_me_data {
   __typename: "UserProfileAll";
   id: string;
   username: string | null;
+  dogname: string | null;
+  photo: string | null;
+  subscribings: number;
+  subscribers: number;
 }
 
 export interface QMeAll_me {
