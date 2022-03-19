bc4b8fff29fb0d7ef25fdcd790c855f53c5146f2
HAKJOON0223 <2hakjoon@gmail.com>
2022-03-19 16:51:58 +0900

    add getMyLikeedPost to apollo cache

--- src/apollo-setup.ts
+++ src/apollo-setup.ts
@@ -3,7 +3,8 @@ import { getAccessToken } from "utils/local-storage";
 import { onError } from "@apollo/client/link/error";
 import { QGetUserPosts_getUserPosts } from "__generated__/QGetUserPosts";
 import { QGetSubscribingPosts_getSubscribingPosts } from "__generated__/QGetSubscribingPosts";
-import { QGetPostsByAddressVariables, QGetPostsByAddress_getPostsByAddress } from "__generated__/QGetPostsByAddress";
+import { QGetPostsByAddress_getPostsByAddress } from "__generated__/QGetPostsByAddress";
+import { QGetMyLikedPosts_getMyLikedPosts } from "__generated__/QGetMyLikedPosts";
 
 
 
@@ -85,6 +86,21 @@ export const cache = new InMemoryCache({
           },
           keyArgs: ['@connection',['key']],
           merge(existing = {data:[]} , incomming:QGetPostsByAddress_getPostsByAddress) {
+            console.log("merge", existing, incomming)
+            return {__typename : incomming.__typename, data:[...existing.data, ...incomming.data]};
+          },
+        },
+        getMyLikedPosts:{
+          read(existing, {args:{ page:{limit, offset}}}:any) {
+            if(!existing){
+              return undefined
+            }
+            console.log("read", existing, existing.data.slice(offset, limit), offset, limit)
+            
+            return {__typename: existing.__typename, error:null, ok:true, data: existing.data.slice(offset, limit)};
+          },
+          keyArgs: false,
+          merge(existing = {data:[]} , incomming:QGetMyLikedPosts_getMyLikedPosts) {
             console.log("merge", existing, incomming)
             return {__typename : incomming.__typename, data:[...existing.data, ...incomming.data]};
           },
