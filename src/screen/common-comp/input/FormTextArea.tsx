a14b700aab3460934cfa3e4737319630a70b1051
HAKJOON0223 <2hakjoon@gmail.com>
2022-04-09 23:19:12 +0900

    add max len

--- src/screen/common-comp/input/FormTextArea.tsx
+++ src/screen/common-comp/input/FormTextArea.tsx
@@ -15,10 +15,11 @@ const TextAreaComponent = styled.textarea<ITextAreaComponent>`
 
 interface IFormTextArea extends ITextAreaComponent {
   register: any;
+  maxLen?: number;
 }
 
 // Todo: auto sizing
-function FormTextArea({ register, height, minHeight }: IFormTextArea) {
+function FormTextArea({ register, height, minHeight, maxLen }: IFormTextArea) {
   // const [textAreaHeight, setTextAreaHeight] = useState<string>('50px');
 
   return (
@@ -27,6 +28,7 @@ function FormTextArea({ register, height, minHeight }: IFormTextArea) {
       placeholder=""
       height={height}
       minHeight={minHeight}
+      maxLength={maxLen}
       // onChange={({ target }) => {
       //   setTextAreaHeight(`${target.scrollHeight - 4}px`);
       // }}
@@ -38,6 +40,7 @@ function FormTextArea({ register, height, minHeight }: IFormTextArea) {
 FormTextArea.defaultProps = {
   height: '300px',
   minHeight: '200px',
+  maxLen: 299,
 };
 
 export default FormTextArea;
