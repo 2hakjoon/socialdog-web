ebde8a88f69832a76b5091af9cc83a60b197f58c
HAKJOON0223 <2hakjoon@gmail.com>
2022-04-15 17:50:31 +0900

    error fix

---
+++ src/screen/home/templates/SubscribingsTemplate.tsx
@@ -0,0 +1,50 @@
+import { gql, useQuery } from '@apollo/client';
+import React, { useEffect, useState } from 'react';
+import WrapperColumn from 'screen/common-comp/wrappers/WrapperColumn';
+import { GET_SUBSCRIBING_POSTS } from 'apllo-gqls/posts';
+import WrapperInfinityScroll from 'screen/common-comp/wrappers/WrapperInfinityScroll';
+import { QGetSubscribingPosts } from '__generated__/QGetSubscribingPosts';
+import PostCard from '../components/PostCard';
+import PostCardLoading from '../components/PostCardLoading';
+import NoContents from 'screen/common-comp/no-contents/NoContents';
+import { CursorArgs } from '__generated__/globalTypes';
+import WrapperInfinityQueryScroll from 'screen/common-comp/wrappers/WrapperInfinityQueryScroll';
+
+function SubscribingsTemplate() {
+  const pageItemCount = 6;
+  const [itemLimit, setItemLimit] = useState(pageItemCount);
+  const getSubscribingPosts = useQuery<QGetSubscribingPosts>(GET_SUBSCRIBING_POSTS, {
+    variables: {
+      page: {
+        take: itemLimit,
+      },
+    },
+    notifyOnNetworkStatusChange: true,
+    fetchPolicy: 'cache-first',
+    onError: (e) => console.log(e),
+  });
+
+  const posts = getSubscribingPosts.data?.getSubscribingPosts.data;
+
+  return (
+    <WrapperColumn p={'0 8px'}>
+      <WrapperInfinityQueryScroll
+        query={getSubscribingPosts}
+        pageItemCount={pageItemCount}
+        setItemLimit={setItemLimit}
+        itemLimit={itemLimit}
+      >
+        {posts?.map((post, idx) => (
+          <PostCard key={post.id} {...post} />
+        ))}
+        {getSubscribingPosts.loading &&
+          Array(pageItemCount)
+            .fill('')
+            .map(() => <PostCardLoading key={Math.random()} />)}
+      </WrapperInfinityQueryScroll>
+      {!getSubscribingPosts.loading && !posts?.length && <NoContents />}
+    </WrapperColumn>
+  );
+}
+
+export default SubscribingsTemplate;
