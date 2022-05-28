48e72ac4844f0f4b6ce04f84aa0ee9238984f25c
HAKJOON0223 <2hakjoon@gmail.com>
2022-05-28 23:01:09 +0900

    add border bottom

--- src/screen/home/HomeScreen.tsx
+++ src/screen/home/HomeScreen.tsx
@@ -2,15 +2,28 @@ import React, { useState } from 'react';
 import MainHeader from 'screen/common-comp/header/MainHeader';
 import BaseWrapper from 'screen/common-comp/wrappers/BaseWrapper';
 import WrapperRow from 'screen/common-comp/wrappers/WrapperRow';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapLocationDot, faUserGroup } from '@fortawesome/free-solid-svg-icons';
 import { theme } from 'assets/styles/theme';
 import AddressPostsTemplate from './templates/AddressPostsTemplate';
 import SubscribingsTemplate from './templates/SubscribingsTemplate';
 import TextBase, { Text } from 'screen/common-comp/texts/TextBase';
+import styled from 'styled-components';
 
 const ADDRESS = 'ADDRESS';
 const SUBSCRIBING = 'SUBSCRIBING';
+
+interface BorderWrapperProps {
+  color?: string;
+  onClick: () => void;
+}
+
+const BorderWrapper = styled.div<BorderWrapperProps>`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: ${(p) => p.color} 4px solid;
+`;
 
 function HomeScreen() {
   const [selectedTab, setSelectedTab] = useState<'ADDRESS' | 'SUBSCRIBING'>(SUBSCRIBING);
@@ -26,12 +39,12 @@ function HomeScreen() {
       <MainHeader />
       <BaseWrapper p={''}>
         <WrapperRow bc={'white'} w="100%" h="60px" jc="space-around">
-          <WrapperRow onClick={() => setSelectedTab(ADDRESS)}>
-            <TextBase text={'지역별'} color={tabIconColor(ADDRESS)} />
-          </WrapperRow>
-          <WrapperRow onClick={() => setSelectedTab(SUBSCRIBING)}>
-            <TextBase text={'구독중'} color={tabIconColor(SUBSCRIBING)} />
-          </WrapperRow>
+          <BorderWrapper color={tabIconColor(ADDRESS)} onClick={() => setSelectedTab(ADDRESS)}>
+            <TextBase text={'지역별'} fontSize={'1.125rem'} fontWeight={500} color={tabIconColor(ADDRESS)} />
+          </BorderWrapper>
+          <BorderWrapper color={tabIconColor(SUBSCRIBING)} onClick={() => setSelectedTab(SUBSCRIBING)}>
+            <TextBase text={'구독중'} fontSize={'1.125rem'} fontWeight={500} color={tabIconColor(SUBSCRIBING)} />
+          </BorderWrapper>
         </WrapperRow>
         {selectedTab === ADDRESS && <AddressPostsTemplate />}
         {selectedTab === SUBSCRIBING && <SubscribingsTemplate />}
