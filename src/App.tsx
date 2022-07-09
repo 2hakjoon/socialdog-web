2b3139e372641615b09f8b05b3a7b451d7099bda
HAKJOON0223 <2hakjoon@gmail.com>
2022-07-10 00:02:38 +0900

    add Suspense for code spliting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { lazy, Suspense, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/styles/theme';
@@ -7,14 +7,15 @@ import ProfileScreen from 'screen/profile/ProfileScreen';
 import HomeScreen from 'screen/home/HomeScreen';
 import { useReactiveVar } from '@apollo/client';
 import { loginState } from './apollo-setup';
-import ProfileEditScreen from 'screen/profile/ProfileEditScreen';
-import SearchScreen from 'screen/search/SearchScreen';
-import PostWriteScreen from 'screen/post-write/PostWriteScreen';
-import PostDetailScreen from 'screen/post-detail/PostDetailScreen';
-import CommentDetailScreen from 'screen/comment-detail/CommentDetailScreen';
-import LoginScreen from 'screen/login/LoginScreen';
-import ScrollToTop from 'screen/common-comp/scroll/ScrollToTop';
-import GeolocationComp from 'screen/common-comp/geolocation/GeolocationComp';
+
+const ProfileEditScreen = lazy(() => import('screen/profile/ProfileEditScreen'));
+const SearchScreen = lazy(() => import('screen/search/SearchScreen'));
+const PostWriteScreen = lazy(() => import('screen/post-write/PostWriteScreen'));
+const PostDetailScreen = lazy(() => import('screen/post-detail/PostDetailScreen'));
+const CommentDetailScreen = lazy(() => import('screen/comment-detail/CommentDetailScreen'));
+const LoginScreen = lazy(() => import('screen/login/LoginScreen'));
+const ScrollToTop = lazy(() => import('screen/common-comp/scroll/ScrollToTop'));
+const GeolocationComp = lazy(() => import('screen/common-comp/geolocation/GeolocationComp'));
 
 window.Kakao.init(process.env.REACT_APP_KAKAO_LOGIN_API_KEY);
 window.Kakao.isInitialized();
@@ -24,25 +25,27 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <ScrollToTop />
-        <GeolocationComp />
-        <Routes>
-          {isLoggedIn ? (
-            <>
-              <Route path={routes.home} element={<HomeScreen />} />
-              <Route path={routes.postDetail} element={<PostDetailScreen />} />
-              <Route path={routes.postWrite} element={<PostWriteScreen />} />
-              <Route path={routes.profile} element={<ProfileScreen />} />
-              <Route path={routes.profileEdit} element={<ProfileEditScreen />} />
-              <Route path={routes.search} element={<SearchScreen />} />
-              <Route path={routes.commentDetail} element={<CommentDetailScreen />} />
-            </>
-          ) : (
-            <>
-              <Route path="*" element={<LoginScreen />} />
-            </>
-          )}
-        </Routes>
+        <Suspense fallback={<></>}>
+          <ScrollToTop />
+          <GeolocationComp />
+          <Routes>
+            {isLoggedIn ? (
+              <>
+                <Route path={routes.home} element={<HomeScreen />} />
+                <Route path={routes.postDetail} element={<PostDetailScreen />} />
+                <Route path={routes.postWrite} element={<PostWriteScreen />} />
+                <Route path={routes.profile} element={<ProfileScreen />} />
+                <Route path={routes.profileEdit} element={<ProfileEditScreen />} />
+                <Route path={routes.search} element={<SearchScreen />} />
+                <Route path={routes.commentDetail} element={<CommentDetailScreen />} />
+              </>
+            ) : (
+              <>
+                <Route path="*" element={<LoginScreen />} />
+              </>
+            )}
+          </Routes>
+        </Suspense>
       </Router>
     </ThemeProvider>
   );
